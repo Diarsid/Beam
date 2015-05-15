1209fff29894a13cc704be28a800d32e6b3b232f
Diarsid <diarsid@gmail.com>
2015-05-15 14:11:49 +0300

    Upgrade.

--- src/com/drs/beam/console/ConsoleManager.java
+++ src/com/drs/beam/console/ConsoleManager.java
@@ -16,62 +16,45 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-
 public class ConsoleManager{
-    // Fields ----------------------------------------------------------------------------------------------------------
-    private Registry registry;
+    // Fields =============================================================================
     private final ConfigReader config = ConfigReader.getReader();
     private final Console console;
 
-    // Constructors ----------------------------------------------------------------------------------------------------
+    // Constructor ========================================================================
     ConsoleManager(Console console) {
         this.console = console;
     }
 
-    // Methods =========================================================================================================
-
+    // Methods ============================================================================
 
     void connect(){
-        prepareRegistry();
         export();
         searchForOrganizer();
     }
-
-    // Methods aocessing ----------------------------------------------------------------
-    private void prepareRegistry(){
+    
+    private void export(){
         if (System.getSecurityManager()==null)
             System.setSecurityManager(new SecurityManager());
-        try {
-            registry = LocateRegistry.createRegistry(config.getConsolePort());
-        }catch (RemoteException re){
-            //re.printStackTrace();
-            //System.out.println(re.getMessage());
-            showProblemMessageAndExit("Console export failure: RemoteException");
-        }
-    }
-
-    private void export(){
         try{
+            int port = config.getConsolePort();
+            Registry registry = LocateRegistry.createRegistry(port);
             ExternalIOIF consoleStub =
-                    (ExternalIOIF) UnicastRemoteObject.exportObject(console, config.getConsolePort());
+                    (ExternalIOIF) UnicastRemoteObject.exportObject(console, port);
             registry.bind(config.getConsoleName(), consoleStub);
         }catch (AlreadyBoundException abe){
-            //abe.printStackTrace();
-            //System.out.println(abe.getMessage());
             showProblemMessageAndExit("Console export failure: AlreadyBoundException");
-        }catch (RemoteException re){
-            //re.printStackTrace();
-            //System.out.println(re.getMessage());
+        }catch (RemoteException re){       
             showProblemMessageAndExit("Console export failure: RemoteException");
         }
     }
+    
     private void searchForOrganizer() {
         if (System.getSecurityManager()==null)
             System.setSecurityManager(new SecurityManager());
         try{
-            registry = LocateRegistry
-                    .getRegistry(config.getOrganizerHost(), config.getOrganizerPort());
-
+            Registry registry = LocateRegistry
+                    .getRegistry(config.getOrganizerHost(), config.getOrganizerPort());                
             OrgIOIF orgIO = (OrgIOIF) registry.lookup(config.getOrgIOName());
             if(orgIO.hasExternalIOProcessor()){
                 showProblemMessageAndExit("Organizer already has external output!");
@@ -83,8 +66,8 @@ public class ConsoleManager{
                 console.setTaskManager((TaskManagerIF) registry.lookup(config.getTaskManagerName()));
             }
         } catch (NotBoundException e){
-            //e.printStackTrace();
-            //System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
+            e.printStackTrace();            
             showProblemMessageAndExit("Connecting to Organizer failure: NotBoundException");
         } catch (RemoteException re){
             //re.printStackTrace();
@@ -93,66 +76,6 @@ public class ConsoleManager{
         }
     }
  
-     /*   
-    void connect(){
-        prepareRegistry();
-        export();
-        searchForOrganizer();
-    }
-
-    // Methods aocessing ----------------------------------------------------------------
-    private void prepareRegistry(){
-        if (System.getSecurityManager()==null)
-            System.setSecurityManager(new SecurityManager());
-        try {
-            registry = LocateRegistry.createRegistry(23102);
-        }catch (RemoteException re){
-            //re.printStackTrace();
-            //System.out.println(re.getMessage());
-            showProblemMessageAndExit("Console export failure: RemoteException");
-        }
-    }
-
-    private void export(){
-        try{
-            ExternalIOIF consoleStub =
-                    (ExternalIOIF) UnicastRemoteObject.exportObject(this, 23102);
-            registry.bind("console", consoleStub);
-        }catch (AlreadyBoundException abe){
-            //abe.printStackTrace();
-            //System.out.println(abe.getMessage());
-            showProblemMessageAndExit("Console export failure: AlreadyBoundException");
-        }catch (RemoteException re){
-            //re.printStackTrace();
-            //System.out.println(re.getMessage());
-            showProblemMessageAndExit("Console export failure: RemoteException");
-        }
-    }
-    private void searchForOrganizer() {
-        try{
-            registry = LocateRegistry
-                    .getRegistry("127.0.0.1", 23101);
-
-            OrgIOIF orgIO = (OrgIOIF) registry.lookup("orgIO");
-            if(orgIO.hasExternalIOProcessor()){
-                showProblemMessageAndExit("Organizer already has external output!");
-            } else{
-                orgIO.acceptNewIOProcessor();
-                console.setOrgIO(orgIO);
-                console.setOsExecutor((OSExecutorIF) registry.lookup("OSExecutor"));
-                console.setTaskManager((TaskManagerIF) registry.lookup("TaskManager"));
-            }
-        } catch (NotBoundException e){
-            //e.printStackTrace();
-            //System.out.println(e.getMessage());
-            showProblemMessageAndExit("Connecting to Organizer failure: NotBoundException");
-        } catch (RemoteException re){
-            //re.printStackTrace();
-            //System.out.println(re.getMessage());
-            showProblemMessageAndExit("Connecting to Organizer failure: RemoteException");
-        }
-    }
-    */ 
     private void showProblemMessageAndExit(String message){
         System.out.println();
         System.out.println(message);
